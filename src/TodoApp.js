6417da912822fef0f5c5f8fa9673325c821c0bff
aravindravi <aravindravi@techmango.net>
2023-09-06 14:16:49 +0530

    implemented unit test for todo CURD

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,9 +6,17 @@ function TodoApp() {
 
   const addTask = () => {
     if (input) {
-      setTasks([...tasks, input]);
+      setTasks([...tasks, { id: Date.now(), text: input }]);
       setInput('');
     }
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const updateTask = (id, newText) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, text: newText } : task));
   };
 
   return (
@@ -20,8 +28,12 @@ function TodoApp() {
       />
       <button onClick={addTask} data-testid="add-task-button">Add</button>
       <ul data-testid="task-list">
-        {tasks.map((task, index) => (
-          <li key={index} data-testid="task-item">{task}</li>
+        {tasks.map(task => (
+          <li key={task.id} data-testid="task-item">
+            {task.text}
+            <button onClick={() => deleteTask(task.id)} data-testid="delete-task-button">Delete</button>
+            <button onClick={() => updateTask(task.id, prompt('New text:', task.text))} data-testid="update-task-button">Update</button>
+          </li>
         ))}
       </ul>
     </div>
--- src/__tests__/TodoApp.test.js
+++ src/__tests__/TodoApp.test.js
@@ -1,16 +1,57 @@
+/* eslint-disable testing-library/prefer-presence-queries */
 /* eslint-disable testing-library/no-unnecessary-act */
 import React from "react"
-import {render, fireEvent, screen, act } from "@testing-library/react"
+import {render, fireEvent, screen, act, getByTestId } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import TodoApp from "../TodoApp"
 
-test("can add tasks to the list",()=>{
+test("Adding to list",()=>{
+render(<TodoApp/>)
+const inputbox=screen.getByTestId("task-input")
+const addButton=screen.getByTestId("add-task-button")
+act(()=>{
+    userEvent.type(inputbox,"to do 1st")
+    fireEvent.click(addButton)
+})
+expect(screen.getByText("to do 1st")).toBeInTheDocument()
+
+})
+
+test("Deleting from the list",()=>{
     render(<TodoApp/>)
-    const input=screen.getByTestId("task-input")
-    const addButton=screen.getByTestId("add-task-button")
+   
+    
     act(()=>{
-        userEvent.type(input,"learn React Testing");
+        const inputbox=screen.getByTestId("task-input")
+        const addButton=screen.getByTestId("add-task-button")
+        userEvent.type(inputbox,"first item")
         fireEvent.click(addButton)
     })
-    expect(screen.getByText("learn React Testing")).toBeInTheDocument()
+    expect(screen.getByText("first item")).toBeInTheDocument()  
+   act(()=>{
+    const deleteButton=screen.getByTestId("delete-task-button")
+    fireEvent.click(deleteButton)
+   })
+   expect(screen.queryByText('first item')).not.toBeInTheDocument();
+    // expect(screen.getByText("first item")).not.toBeInTheDocument()  
+
+})
+
+test("Update action",()=>{
+    render(<TodoApp/>)
+    const inputbox=screen.getByTestId("task-input")
+    const addButton=screen.getByTestId("add-task-button")
+    act(()=>{
+        userEvent.type(inputbox,"first item")
+        fireEvent.click(addButton)
+    })
+    expect(screen.getByText("first item")).toBeInTheDocument()
+
+    act(()=>{
+        const updateButton=screen.getByTestId("update-task-button")
+        window.prompt=jest.fn(()=>"update the text")
+        fireEvent.click(updateButton)
+    })
+    expect(screen.queryByText("update the text")).toBeInTheDocument()
+
 })
